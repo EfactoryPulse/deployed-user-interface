208da83e4150758bc9b41cba1b56031e659b0bba
deploy <github@efactorypulse.com>
2025-01-04 12:51:46 +0000

    release

---
+++ _nuxt/Cr08p5Za.js
@@ -0,0 +1,1 @@
+import"./DQbpM_yT.js";const r=""+new URL("Logo.DyW9fXLI.png",import.meta.url).href;export{r as _};
---
+++ _nuxt/DvEoa95S.js
@@ -0,0 +1,1 @@
+import{r as e,c as a,o as c}from"./DQbpM_yT.js";const l={__name:"machine1",setup(r){return e(!1),(n,o)=>(c(),a("div",null," hello Machine 1 "))}};export{l as default};
